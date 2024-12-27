191b5940e78da79ef3f6167e8cdc3bfc2dbed71e
unknown <mehetajkhandaker@gmail.com>
2024-12-27 22:21:06 +0600

    formSelectInput type error solved

--- components/FormInputs/FormSelectInput.tsx
+++ components/FormInputs/FormSelectInput.tsx
@@ -3,15 +3,17 @@ import AddNewButton from "@/components/FormInputs/AddNewButton";
 import React from "react";
 import Select from "react-tailwindcss-select";
 import { Option, Options } from "react-tailwindcss-select/dist/components/type";
+
 type FormSelectInputProps = {
-  options: Options;
-  label: string;
-  option: Option;
-  setOption: any;
-  href?: string;
-  labelShown?: boolean;
-  toolTipText?: string;
+  options: Options; // Array of options for the Select component
+  label: string; // Label for the input
+  option: Option | null; // Currently selected option
+  setOption: (option: Option | null) => void; // Function to update the selected option
+  href?: string; // Optional URL for the AddNewButton
+  labelShown?: boolean; // Whether to show the label
+  toolTipText?: string; // Tooltip text for the AddNewButton
 };
+
 export default function FormSelectInput({
   options,
   label,
@@ -33,7 +35,7 @@ export default function FormSelectInput({
           isSearchable
           primaryColor="blue"
           value={option}
-          onChange={(item) => setOption(item)}
+          onChange={(item) => setOption(item as Option | null)}
           options={options}
           placeholder={label}
         />
